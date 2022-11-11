c207062d83fde4524898d258fcde64037c35dee7
tuantvhisoft <tuantv@hisoft.vn>
2022-11-11 15:22:36 +0700

    comit

--- DigitalSignature/Program.cs
+++ DigitalSignature/Program.cs
@@ -1,20 +1,63 @@
+using Digital.Data.Data;
+using Digital_BE.Api.Extensions;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.ConfigureSwagger();
+builder.Services.AddAutoMapper();
+builder.Services.AddJWTAuthentication(builder.Configuration);
+builder.Services.ConfigCors();
+builder.Services.AddDbContext(builder.Configuration);
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddBusinessService();
+
+builder.Services.Configure<FormOptions>(x =>
+{
+    x.ValueLengthLimit = int.MaxValue;
+    x.BufferBodyLengthLimit = int.MaxValue;
+    x.MultipartHeadersLengthLimit = int.MaxValue;
+    x.MultipartBodyLengthLimit = int.MaxValue; // In case of multipart
+});
 
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+
+}
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
+app.UseCors("AllowAll");
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseAuthentication();
+
+
+app.Services.ApplyPendingMigrations();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<ApplicationDBContext>();
+    if (context.Database.GetPendingMigrations().Any())
+    {
+        context.Database.Migrate();
+    }
+}
 
 app.MapControllers();
 
