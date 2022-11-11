13c5a2e077a718ca307159e89ca6f703d257a66e
tuantvhisoft <tuantv@hisoft.vn>
2022-11-11 22:24:48 +0700

    comit

--- Digital-BE/Program.cs
+++ Digital-BE/Program.cs
@@ -51,15 +51,18 @@ app.Services.ApplyPendingMigrations();
 
 using (var scope = app.Services.CreateScope())
 {
-   var services = scope.ServiceProvider;
+    var services = scope.ServiceProvider;
 
-   var context = services.GetRequiredService<ApplicationDBContext>();
-   if (context.Database.GetPendingMigrations().Any())
-   {
-       context.Database.Migrate();
-   }
+    var context = services.GetRequiredService<ApplicationDBContext>();
+    if (context.Database.GetPendingMigrations().Any())
+    {
+        context.Database.Migrate();
+    }
 }
 
-app.MapControllers();
+app.UseEndpoints(endpoints =>
+           {
+               endpoints.MapControllers();
+           });
 
 app.Run();
