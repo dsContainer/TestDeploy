70b51bde6ec45da115556b28c57759673544c84f
tuantvhisoft <tuantv@hisoft.vn>
2022-11-11 22:23:23 +0700

    comit

--- Digital-BE/Program.cs
+++ Digital-BE/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 builder.Services.ConfigureSwagger();
 builder.Services.AddAutoMapper();
-// builder.Services.AddJWTAuthentication(builder.Configuration);
+builder.Services.AddJWTAuthentication(builder.Configuration);
 builder.Services.ConfigCors();
 builder.Services.AddDbContext(builder.Configuration);
 builder.Services.AddHttpContextAccessor();
@@ -44,21 +44,21 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-// app.UseAuthentication();
+app.UseAuthentication();
 
 
-// app.Services.ApplyPendingMigrations();
+app.Services.ApplyPendingMigrations();
 
-// using (var scope = app.Services.CreateScope())
-// {
-//    var services = scope.ServiceProvider;
+using (var scope = app.Services.CreateScope())
+{
+   var services = scope.ServiceProvider;
 
-//    var context = services.GetRequiredService<ApplicationDBContext>();
-//    if (context.Database.GetPendingMigrations().Any())
-//    {
-//        context.Database.Migrate();
-//    }
-// }
+   var context = services.GetRequiredService<ApplicationDBContext>();
+   if (context.Database.GetPendingMigrations().Any())
+   {
+       context.Database.Migrate();
+   }
+}
 
 app.MapControllers();
 
