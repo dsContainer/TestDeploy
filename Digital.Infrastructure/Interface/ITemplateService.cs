a697a9b9f9129ebfe9d565d84e3fa98fc6ea8b95
melloDr <thanhlong020920@gmail.com>
2022-11-13 17:16:05 +0700

    Commit GetAll and UploadTemplate without save template

---
+++ Digital.Infrastructure/Interface/ITemplateService.cs
@@ -0,0 +1,16 @@
+﻿using Digital.Infrastructure.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Digital.Infrastructure.Model.TemplateModel;
+
+namespace Digital.Infrastructure.Interface
+{
+    public interface ITemplateService
+    {
+        Task<ResultModel> GetTemplate();
+        Task<ResultModel> UploadTemplate(TemplateModel model, Guid documentTypeId);
+    }
+}
